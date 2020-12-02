c2a17d55425ade857c3e7112686d3cb38fa3a707
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-12-02 15:25:25 +0900

    feat: get videoId from url

--- fetchInfoFromYouTube.js
+++ fetchInfoFromYouTube.js
@@ -4,11 +4,35 @@ firstScript = document.getElementsByTagName( 'script' )[ 0 ];
 firstScript.parentNode.insertBefore( script , firstScript );
 
 let player;
+const videoId = getVideoId();
+
+function getVideoId() {
+    const para = getUrlVars();
+    if (para["v"]) {
+        return para["v"];
+    } else {
+        return "o1jIYNyr0fs";
+    }
+}
+
+function getUrlVars(){
+    var vars = {};
+    var param = location.search.substring(1).split('&');
+    for(var i = 0; i < param.length; i++) {
+        var keySearch = param[i].search(/=/);
+        var key = '';
+        if(keySearch != -1) key = param[i].slice(0, keySearch);
+        var val = param[i].slice(param[i].indexOf('=', 0) + 1);
+        if(key != '') vars[key] = decodeURIComponent(val);
+    }
+    return vars;
+}
+
 function onYouTubeIframeAPIReady() {
     player = new YT.Player('player', {
         height: '360',
         width: '640',
-        videoId: 'o1jIYNyr0fs',
+        videoId: videoId,
         events: {
             'onReady': onPlayerReady,
             // 'onStateChange': onPlayerStateChange

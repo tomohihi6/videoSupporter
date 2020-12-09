edae38cb8558ca930d77e314ddee237f9475a50d
Tomohiro Kawatani <b1017111@fun.ac.jp>
2020-12-09 15:34:06 +0900

    feat: display modal when upload is finished

--- public/js/main.js
+++ public/js/main.js
@@ -319,7 +319,7 @@ function uploadFileToFirebaseStorage(fileName) {
         // For instance, get the download URL: https://firebasestorage.googleapis.com/...
         uploadTask.snapshot.ref.getDownloadURL().then(function(downloadURL) {
             console.log('File available at', downloadURL);
-            alert("アップロードされました");
+            openModal();
         });
     });
 }
@@ -391,3 +391,62 @@ function addHlText(highlightContentsNum) {
     const returnTexts = editorDoc.getValue() + highlightTexts;
     editorDoc.setValue(returnTexts);
 }
+
+function openModal() {
+    if($("#modal-overlay")[0]) return false ;		//新しくモーダルウィンドウを起動しない [下とどちらか選択]
+    //if($("#modal-overlay")[0]) $("#modal-overlay").remove() ;		//現在のモーダルウィンドウを削除して新しく起動する [上とどちらか選択]
+
+    //オーバーレイ用のHTMLコードを、[body]内の最後に生成する
+    $("body").append('<div id="modal-overlay"></div>');
+
+    //[$modal-overlay]をフェードインさせる
+    $("#modal-overlay").fadeIn("slow");
+
+    $("#modal-overlay").click(closeModal);
+
+    //[$modal-content]をフェードインさせる
+    $("#modal-content").fadeIn("slow");
+
+    const mainUrl = `https://myresearch.firebaseapp.com/ob.html?v=${videoId}`;
+
+    $("#modal-texts").html(`アップロードが完了しました．\n作成したスクリプトの動作は<a href="${mainUrl}">こちら</a>で確認できます．`);
+    centeringModalSyncer();
+}
+
+function centeringModalSyncer(){
+
+	//画面(ウィンドウ)の幅を取得し、変数[w]に格納
+	var w = $(window).width();
+    console.log(w);
+	//画面(ウィンドウ)の高さを取得し、変数[h]に格納
+	var h = $(window).height();
+
+	//コンテンツ(#modal-content)の幅を取得し、変数[cw]に格納
+    var cw = $("#modal-content").outerWidth();
+    console.log(cw);
+
+	//コンテンツ(#modal-content)の高さを取得し、変数[ch]に格納
+	var ch = $("#modal-content").outerHeight();
+
+	//コンテンツ(#modal-content)を真ん中に配置するのに、左端から何ピクセル離せばいいか？を計算して、変数[pxleft]に格納
+	var pxleft = ((w - cw)/2);
+
+	//コンテンツ(#modal-content)を真ん中に配置するのに、上部から何ピクセル離せばいいか？を計算して、変数[pxtop]に格納
+    var pxtop = ((h - ch)/2);
+    console.log(`pxleft is ${pxleft}\n pxtop is ${pxtop}`)
+
+	//[#modal-content]のCSSに[left]の値(pxleft)を設定
+	$("#modal-content").css({"left": pxleft + "px"});
+
+	//[#modal-content]のCSSに[top]の値(pxtop)を設定
+	$("#modal-content").css({"top": pxtop + "px"});
+
+}
+
+function closeModal() {
+    $("#modal-content,#modal-overlay").fadeOut("slow",function(){
+        //フェードアウト後、[#modal-overlay]をHTML(DOM)上から削除
+        $("#modal-overlay").remove();
+    });
+}
+
